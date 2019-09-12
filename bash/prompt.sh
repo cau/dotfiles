6fcc37caab3b9b4d349119506a4254f4ee6a7c70
Douglas Cau <dc@dynamicsource.se>
2019-09-12 15:39:49 +0200

    Adding colors to promt

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,11 +1,27 @@
 function __branch {
-  echo "`git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/ \(\1\)/'`"
+  echo -e "`git branch 2>/dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/'`"
+}
+
+function __repository {
+  BRANCH="$(__branch)"
+
+  if [ ! -z "${BRANCH}" ]
+  then
+
+  if [ "${BRANCH}" = "master" ]; then
+    BRANCH_COLOR="\033[31m"
+  else
+    BRANCH_COLOR="\033[33m"
+  fi
+
+      echo -e " (${BRANCH_COLOR}${BRANCH}\033[0m)"
+  fi
 }
 
 function __server {
   if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-    echo "`whoami`@`hostname -s` "
+    echo -e "\033[37m`whoami`\033[0m@\033[37m`hostname -s`\033[0m "
   fi
 }
 
-export PS1="\$(__server)\w\$(__branch) \$ ";
+export PS1="\$(__server)\033[34m\w\033[0m\$(__repository) \$ ";
