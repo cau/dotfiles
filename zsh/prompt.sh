79e9b47376d7cde7c11ef4997cb658aa513e07cb
Douglas Cau <douglas@cau.se>
2023-03-24 15:44:28 +0100

    Adding git branch to prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,1 +1,6 @@
-export PROMPT="%~ %# "
+parse_git_branch() {
+    git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+setopt PROMPT_SUBST
+export PROMPT='%F{blue}%~%f%F{red}$(parse_git_branch)%f %# '
