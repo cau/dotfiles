ec7b28ef1e6d4198a62f01cbeaac2916ee24ed38
Douglas Cau <douglas@cau.se>
2025-09-26 14:04:43 +0200

    Enable arrow up/down history search in zsh

--- zsh/terminal.sh
+++ zsh/terminal.sh
@@ -5,3 +5,8 @@ compinit
 
 eval "$(op completion zsh)"; compdef _op op
 
+autoload -U history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^[[A" history-beginning-search-backward-end
+bindkey "^[[B" history-beginning-search-forward-end
